f2bc383e6b72c1886de190a66379c37348ad545a
Andrey Yakovenko <aaykovenko@yandex.ru>
2021-02-19 03:15:38 +0400

    update tests

--- tests/simple.spec.ts
+++ tests/simple.spec.ts
@@ -1,5 +1,18 @@
 import Parser from '../src';
 
+const textForTest = `public class Address
+{
+    public int Id {get; set;}
+    public string Street { get; set; }
+    public string City { get; set; }
+}`
+
+const textForResult = `export interface Address
+{
+    Id: number;
+    Street: string;
+    City: string;
+}`
 
 describe('Simple tests', () => {
     const parser = new Parser();
@@ -7,4 +20,8 @@ describe('Simple tests', () => {
     test('Init Parser class', () => {
         expect(parser).toBeInstanceOf(Parser);
     });
+
+    test('Parse easy model', () => {
+        expect(parser.parse(textForTest)).toBe(textForResult);
+    })
 });
