ebf79032e0e5ee5e62dbb561eb8dcaa7c2804c67
Andrey Yakovenko <aaykovenko@yandex.ru>
2021-02-19 02:57:30 +0400

    added public members generator

--- src/generators/index.ts
+++ src/generators/index.ts
@@ -3,3 +3,4 @@ export * as comments from './comments';
 export * as methods from './methods';
 export * as properties from './properties';
 export * as attributes from './attributes';
+export * as publicMembers from './publicMembers';
