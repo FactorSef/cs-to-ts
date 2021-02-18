948bb50e48d64892f1775dce6ad85313834f3883
Andrey Yakovenko <aaykovenko@yandex.ru>
2021-02-19 03:15:31 +0400

    update jest config

---
+++ jest.config.ts
@@ -0,0 +1,11 @@
+/*
+ * For a detailed explanation regarding each configuration property and type check, visit:
+ * https://jestjs.io/docs/en/configuration.html
+ */
+
+export default {
+  coverageDirectory: "coverage",
+  coverageProvider: "v8",
+  preset: 'ts-jest',
+  testEnvironment: "node",
+};
