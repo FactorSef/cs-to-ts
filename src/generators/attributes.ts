89bd8b84466f4f9872c9f33f52033f5e92e75959
Andrey Yakovenko <aaykovenko@yandex.ru>
2021-02-19 02:50:38 +0400

    added attribute generator

---
+++ src/generators/attributes.ts
@@ -0,0 +1,15 @@
+import { IConfig } from "../config";
+import { IGenerateResult } from "../types";
+
+export function generateAttr(code: string, config: IConfig): IGenerateResult {
+    const pattern = /[ \t]*\[\S*\][ \t]*\r?\n/;
+    const match = pattern.exec(code);
+    if (!match) {
+        return null;
+    }
+    return {
+        result: "",
+        index: match.index,
+        length: match[0].length,
+    }
+}
