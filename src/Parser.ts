2f8021857a5c403e3e20efbbba3c252db72624b3
Andrey Yakovenko <aaykovenko@yandex.ru>
2021-03-12 16:37:43 +0400

    split index file

---
+++ src/Parser.ts
@@ -0,0 +1,93 @@
+import { IConfig, getConfig } from "./config";
+import { GenerateFn, IGenerateResult, ParseFn, BindedParserFn } from "./types";
+import * as parsers from './parsers';
+import * as generators from './generators';
+
+class Parser {
+    /** Configuration parser */
+    private config: IConfig;
+    /** All parser functions */
+    private parsers: Array<BindedParserFn> = [];
+
+    constructor (config?: IConfig) {
+        this.config = getConfig(config);
+        this.bindParsers();
+    }
+
+    bindParsers() {
+        this.parsers = [
+            Parser.fn(parsers.classes.parseClass, generators.classes.generateClass),
+            Parser.fn(parsers.comments.parseXmlDocBlock, generators.comments.generateJsDoc),
+            Parser.fn(parsers.methods.parseConstructor, generators.methods.generateConstructor),
+            Parser.fn(parsers.methods.parseMethod, generators.methods.generateMethod),
+            Parser.fn(parsers.properties.parseProperty, generators.properties.generateProperty),
+            generators.publicMembers.generatePublicMember,
+            generators.attributes.generateAttr,
+        ]
+    }
+
+    parse(code: string) {
+        let index = 0;
+        let result = '';
+
+        while (true) {
+            const nextMatch = this.nextParse(code, index);
+            if (!nextMatch) {
+                break;
+            }
+
+            // add the last unmatched code:
+            result += code.substr(index, nextMatch.index - index);
+            // add the matched code:
+            result += nextMatch.result;
+            // increment the search index:
+            index = nextMatch.index + nextMatch.length;
+        }
+
+        result += code.substr(index);
+
+        return result;
+    }
+
+    /**
+     * Binding parse and generate functions
+     * @param parse parse fn
+     * @param generate generate fn
+     */
+    static fn(parse: ParseFn, generate: GenerateFn): BindedParserFn {
+        return (code: string, config: IConfig): IGenerateResult => {
+            const parseResult = parse(code, config);
+
+            if (!parseResult) {
+                return null;
+            }
+
+            return {
+                result: generate(parseResult.data, config),
+                index: parseResult.index,
+                length: parseResult.length,
+            }
+        }
+    }
+
+    private nextParse(code: string, index: number): IGenerateResult {
+        const trimmedCode = code.substr(index);
+        let firstMatch: IGenerateResult = null;
+
+        this.parsers.forEach((parser) => {
+            const match = parser(trimmedCode, this.config);
+    
+            if (match && (!firstMatch || match.index < firstMatch.index)) {
+                firstMatch = match;
+            }
+        })
+    
+        return firstMatch && {
+            result: firstMatch.result,
+            index: firstMatch.index + index,
+            length: firstMatch.length,
+        };
+    }
+}
+
+export default Parser;
